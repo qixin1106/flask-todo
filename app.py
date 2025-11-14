62c6ffe3dc41edbaab0cbc94ca69e9d0b1c3663a
qixin1106 <63401364@qq.com>
2025-11-14 16:06:51 +0800

    update

--- app.py
+++ app.py
@@ -46,5 +46,7 @@ def delete(todo_id):
     return redirect(url_for("home"))
 
 if __name__ == "__main__":
-    db.create_all()
+    print("Starting the Flask application...")
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
